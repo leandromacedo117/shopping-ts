fdd17ce160b8383503dd8497cc93761c211d680b
Leandro Macedo <leeandromacedo117@gmail.com>
2024-02-24 12:39:45 -0300

    styles

--- shopping-react/src/Item/Item.tsx
+++ shopping-react/src/Item/Item.tsx
@@ -1,1 +1,30 @@
-import Button from 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+// types 
+import { CartItemType } from "../App";
+// styles 
+import { Wrapper } from './Item.styles';
+import './Item.module.css'
+
+type Props = {
+    item: CartItemType;
+    handleAddToCart:(clickedItem: CartItemType) => void;
+}
+
+const Item: React.FC<Props> = ({ item, handleAddToCart}) => (
+    <Wrapper>
+        <span><img src={item.image} alt={item.title} /></span>
+        <div className='item-list'>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <h3>{item.price+'R$'}</h3>
+        </div>
+        <span>
+            <Button variant='primary'
+            onClick={() => handleAddToCart(item)  } 
+            > Add Item </Button>
+        </span>
+    </Wrapper>
+);
+
+export default Item;
